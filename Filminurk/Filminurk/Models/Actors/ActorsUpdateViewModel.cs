065b014e7bc164602aa01ba498f03f7f7ff61862
Opilane TTHK <opilane@tthk.ee>
2025-10-30 11:33:11 +0200

    a

--- Filminurk/Filminurk/Models/Actors/ActorsUpdateViewModel.cs
+++ Filminurk/Filminurk/Models/Actors/ActorsUpdateViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Filminurk.Models.Actors
+﻿using Filminurk.Core.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Actors
 {
     public class ActorsUpdateViewModel
     {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string>? MoviesActedFor { get; set; }
+        public Guid? PortraitID { get; set; }
+        /* 3 enda mõeldud andmetüüpi */
+        public decimal ActorRating { get; set; }
+        public string MovieKnownFor { get; set; }
+        public Genders Gender { get; set; }
     }
 }
