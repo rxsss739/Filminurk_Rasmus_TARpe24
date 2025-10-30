c370533dc31a3f35bed71cae6a57a927accbaae5
Opilane TTHK <opilane@tthk.ee>
2025-10-30 08:46:35 +0200

    Added actor in domain, made gender enum for specifying gender in actor

---
+++ Filminurk/Filminurk.Core/Domain/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public class Actor
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+    }
+}
