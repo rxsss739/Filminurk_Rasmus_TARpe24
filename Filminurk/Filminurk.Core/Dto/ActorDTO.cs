dca19e03f7b0df225e35f9bbf2e0d4437dee1671
Opilane TTHK <opilane@tthk.ee>
2025-10-30 08:49:49 +0200

    Made ActorDTO, added datatypes

---
+++ Filminurk/Filminurk.Core/Dto/ActorDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filminurk.Core.Domain;
+
+namespace Filminurk.Core.Dto
+{
+    public class ActorDTO
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public Guid PortraitID { get; set; }
+        /* 3 enda mõeldud andmetüüpi */
+        public decimal ActorRating { get; set; }
+        public string MovieKnownFor { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
