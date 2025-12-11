9cfa6c5017151f064219a198f861602cd15aaec2
opilane <opilane@HP-CZC3217069>
2025-12-11 11:25:08 +0200

    added all necessary views

--- Filminurk/Filminurk/Models/Accounts/ForgotPasswordViewModel.cs
+++ Filminurk/Filminurk/Models/Accounts/ForgotPasswordViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace Filminurk.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Accounts
 {
     public class ForgotPasswordViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
--- Filminurk/Filminurk/Models/Accounts/ResetPasswordViewModel.cs
+++ Filminurk/Filminurk/Models/Accounts/ResetPasswordViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace Filminurk.Models.Accounts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Accounts
 {
     public class ResetPasswordViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Kirjuta oma uus parool uuesti")]
+        [Compare("Password", ErrorMessage = "Paroolid ei kattu, proovi uuesti.")]
+        public string ConfirmNewPassword { get; set; }
+
+        public string Token { get; set; }
     }
 }
